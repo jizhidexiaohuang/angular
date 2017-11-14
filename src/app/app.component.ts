62b4e48d5fe8a440bd8d158f681d6bcc41bb0943
wf <18390239389@163.com>
2017-11-14 18:16:10 +0800

    提交

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls:['./app.component.css'],
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard">dashboard</a>
+      <a routerLink="/heroes">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  title = 'angular';
 }
